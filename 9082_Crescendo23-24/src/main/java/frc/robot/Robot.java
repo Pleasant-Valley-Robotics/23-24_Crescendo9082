2a0323147e29267b030a99672c724612eaa7a543
Brandon Seamer <brandonseamer@gmail.com>
2024-01-06 17:20:07 -0600

    Removed unnecessary whitespace

--- 9082_Crescendo23-24/src/main/java/frc/robot/Robot.java
+++ 9082_Crescendo23-24/src/main/java/frc/robot/Robot.java
@@ -33,18 +33,23 @@ public class Robot extends TimedRobot {
 
   //Method to allow for a rainbow cascade effect on our LEDs
   private void rainbow() {
+    
     //For every pixel
     for (var i = 0; i < m_LedBuffer.getLength(); i++) {
       //Calculate the hue - hue is easier for rainbows because the color
       //shape is a circle so only one value needs to precess
       final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_LedBuffer.getLength())) % 180;
+    
       //Set the value
       m_LedBuffer.setHSV(i, hue, 255, 128);
     }
+    
     //Increase by to make the rainbow "move"
     m_rainbowFirstPixelHue += 3;
+    
     //Check bounds
     m_rainbowFirstPixelHue %= 180;
+
   } //End of rainbow
 
   @Override
