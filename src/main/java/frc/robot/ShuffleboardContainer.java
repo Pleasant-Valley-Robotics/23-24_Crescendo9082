91f3cc99af146475b0c07feedf6cf17c8382c32e
Will Rages <rageswill@pleasval.org>
2024-02-01 17:13:53 -0600

    Add a shuffleboard container to house all the needed ui elements

---
+++ src/main/java/frc/robot/ShuffleboardContainer.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+public class ShuffleboardContainer {
+    public static ShuffleboardTab robotData = Shuffleboard.getTab("Robot Data");
+}
