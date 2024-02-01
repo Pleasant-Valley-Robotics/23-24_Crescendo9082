84c401d77990514d9a316bcbc0895c5b187357e4
Will Rages <rageswill@pleasval.org>
2024-02-01 17:00:47 -0600

    Redo folder structure

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -57,14 +57,9 @@ public class RobotContainer {
         robotDrive.setDefaultCommand(
                 // A split-stick arcade command, with forward/backward controlled by the left
                 // hand, and turning controlled by the right.
-                new RunCommand(
-                        () ->
-                                robotDrive.drive(driverJoystick.getY(), driverJoystick.getX(), driverJoystick.getTwist(), true), robotDrive));
+                new RunCommand(() -> robotDrive.drive(driverJoystick.getY(), driverJoystick.getX(), driverJoystick.getTwist(), true), robotDrive));
 
-        robotIntake.setDefaultCommand(
-                new RunCommand(
-                        () ->
-                                robotIntake.arm(driverJoystick2.getY()), robotIntake));
+        robotIntake.setDefaultCommand(new RunCommand(() -> robotIntake.arm(driverJoystick2.getY()), robotIntake));
     }
 
     /**
