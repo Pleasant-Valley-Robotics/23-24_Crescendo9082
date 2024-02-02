4b597596883638c5fe075afefaae5d91c0580e51
BritneyBick <148918388+BritneyBick@users.noreply.github.com>
2024-02-01 19:12:43 -0600

    Clean up RobotContainer with another class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,30 +4,17 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.MecanumControllerCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
+import frc.robot.commands.Autos;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.HangingSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
-
-import java.util.List;
 
 /*
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -50,7 +37,16 @@ public class RobotContainer {
      */
     public RobotContainer() {
         configureButtonBindings();
+        configureTeleOpCommands();
+    }
 
+    private double applyDeadzone(double input, double deadzone) {
+        if (input > deadzone) return (input - deadzone) / (1 - deadzone);
+        else if (input < -deadzone) return (input + deadzone) / (1 - deadzone);
+        else return 0.0;
+    }
+
+    private void configureTeleOpCommands() {
         robotDrive.setDefaultCommand(new RunCommand(() -> {
             // Scale throttle from [1.0, -1.0] to [0.0, 1.0].
             double throttle = driverJoystick.getThrottle() / -2 + 0.5;
@@ -68,27 +64,6 @@ public class RobotContainer {
         robotIntake.setDefaultCommand(new RunCommand(() -> robotIntake.arm(driverJoystick2.getY()), robotIntake));
     }
 
-    private double applyDeadzone(double input, double deadzone) {
-        if (input > deadzone) return (input - deadzone) / (1 - deadzone);
-        else if (input < -deadzone) return (input + deadzone) / (1 - deadzone);
-        else return 0.0;
-    }
-
-    private double[] getInputs() {
-        double throttle = driverJoystick.getThrottle() / -2 + 0.5;
-        return new double[]{
-                applyDeadzone(-driverJoystick.getY(), 0.1) * throttle,
-                applyDeadzone(driverJoystick.getX(), 0.1) * throttle,
-                applyDeadzone(driverJoystick.getTwist(), 0.1) * throttle,
-        };
-    }
-
-    /**
-     * Use this method to define your button->command mappings. Buttons can be created by
-     * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling passing it to a
-     * {@link JoystickButton}.
-     */
     private void configureButtonBindings() {
         // Drive at half speed when the 1 button is held.
         new JoystickButton(driverJoystick, 1)
@@ -114,54 +89,6 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        // Create config for trajectory
-        TrajectoryConfig config =
-                new TrajectoryConfig(
-                        AutoConstants.MAX_SPEED_METERS_PER_SECOND,
-                        AutoConstants.MAX_ACCELERATION_METERS_PER_SECOND_SQUARED)
-                        // Add kinematics to ensure max speed is actually obeyed
-                        .setKinematics(DriveConstants.DRIVE_KINEMATICS);
-
-        // An example trajectory to follow.  All units in meters.
-        Trajectory exampleTrajectory =
-                TrajectoryGenerator.generateTrajectory(
-                        // Start at the origin facing the +X direction
-                        new Pose2d(0, 0, new Rotation2d(0)),
-                        // Pass through these two interior waypoints, making an 's' curve path
-                        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-                        // End 3 meters straight ahead of where we started, facing forward
-                        new Pose2d(3, 0, new Rotation2d(0)),
-                        config);
-
-        MecanumControllerCommand mecanumControllerCommand =
-                new MecanumControllerCommand(
-                        exampleTrajectory,
-                        robotDrive::getPose,
-                        DriveConstants.FEEDFORWARD,
-                        DriveConstants.DRIVE_KINEMATICS,
-
-                        // Position controllers
-                        new PIDController(AutoConstants.PX_CONTROLLER, 0, 0),
-                        new PIDController(AutoConstants.PY_CONTROLLER, 0, 0),
-                        new ProfiledPIDController(
-                                AutoConstants.P_THETA_CONTROLLER, 0, 0, AutoConstants.THETA_CONTROLLER_CONSTRAINTS),
-
-                        // Needed for normalizing wheel speeds
-                        AutoConstants.MAX_SPEED_METERS_PER_SECOND,
-
-                        // Velocity PID's
-                        new PIDController(DriveConstants.P_FRONT_LEFT_VEL, 0, 0),
-                        new PIDController(DriveConstants.P_REAR_LEFT_VEL, 0, 0),
-                        new PIDController(DriveConstants.P_FRONT_RIGHT_VEL, 0, 0),
-                        new PIDController(DriveConstants.P_REAR_RIGHT_VEL, 0, 0),
-                        robotDrive::getCurrentWheelSpeeds,
-                        robotDrive::setDriveMotorControllersVolts, // Consumer for the output motor voltages
-                        robotDrive);
-
-        // Reset odometry to the starting pose of the trajectory.
-        robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
-
-        // Run path following command, then stop at the end.
-        return mecanumControllerCommand.andThen(() -> robotDrive.drive(0, 0, 0, false));
+        return Autos.simpleAuto(robotDrive);
     }
 }
